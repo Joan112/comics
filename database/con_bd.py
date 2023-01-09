9e557fc6f42ce2a01d13e0da174b627835dba1fb
joan manuel iribe martinez <jm_im@outlook.com>
2023-01-08 22:43:36 -0700

    actualizacion

--- database/con_bd.py
+++ database/con_bd.py
@@ -2,8 +2,9 @@ import pymongo
 
 client = pymongo.MongoClient("mongodb://tests_python:FdpqmEtaXkkM2Iqx@ac-mbprgjx-shard-00-01.mbzev6i.mongodb.net:27017,ac-mbprgjx-shard-00-01.mbzev6i.mongodb.net:27017,ac-mbprgjx-shard-00-01.mbzev6i.mongodb.net:27017/?authMechanism=DEFAULT&authSource=admin&tls=true")
 db = client.pruebas
+
 db = client["pruebas"]
 users_collection = db["users"]
 users_collection_token = db["token_temp"]
-users_collection_addToLayaway = db["pruebas.addToLayaway"]
+users_collection_addToLayaway = db["addToLayaway"]
 
